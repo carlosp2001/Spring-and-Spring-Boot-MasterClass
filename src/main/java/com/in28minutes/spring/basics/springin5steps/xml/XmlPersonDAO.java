a0182389c14cb16af9b4182d9a60f28cdec50af3
carlosp2001 <pinedalberto@hotmail.com>
2023-11-22 13:56:43 -0600

    Definiendo el spring contexto manualmente mediante un archivo xml

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/xml/XmlPersonDAO.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.springin5steps.xml;
+
+public class XmlPersonDAO {
+    XmlJdbcConnection XmlJdbcConnection;
+
+    public XmlJdbcConnection getXmlJdbcConnection() {
+        return XmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+        XmlJdbcConnection = xmlJdbcConnection;
+    }
+}
