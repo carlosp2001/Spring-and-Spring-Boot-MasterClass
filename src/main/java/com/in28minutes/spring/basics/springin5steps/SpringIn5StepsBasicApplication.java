b8de373589f42a519a87aa078bb6026024772856
carlosp2001 <pinedalberto@hotmail.com>
2023-11-22 11:14:30 -0600

    Ciclo de vida del bean: @PostConstruct y @PreDestroy

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -38,14 +38,13 @@ public class SpringIn5StepsBasicApplication {
         ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-        BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+//        BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
 
         System.out.println(binarySearch);
-        System.out.println(binarySearch1);
+//        System.out.println(binarySearch1);
 
         int result = binarySearch.binarySearch(new int[]{124, 6}, 3);
         System.out.println(result);
-
     }
 
 }
--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,9 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -9,8 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 //@Scope("prototype") // Especificamos el alcance prototipo, esto nos permite poder crear diferentes instacias del bean,
 // cada vez que requerimos una instancia
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Forma correcta de especificar el alcance
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) // Forma correcta de especificar el alcance
 public class BinarySearchImpl {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     // 1- Forma haciendo uso de búsqueda por nombre
 
@@ -51,4 +57,20 @@ public class BinarySearchImpl {
         // Return the result
         return 3;
     }
+
+    // El método postConstruct se invoca una vez se haya creado y populado con las dependencias el bean
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("postConstruct");
+    }
+
+    // Este método se invoca antes de destruirse el bean
+    // Para poder darle seguimiento al ciclo de vida de los beans este debe ser singleton,
+    // spring solo ofrece seguimiento al bean si este singleton, si quisieramos darle seuimiento
+    // al componente deemos hacer un callback
+    @PreDestroy
+    public void preDestroy(){
+        logger.info("Destroyed");
+    }
+
 }
