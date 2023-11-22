1d98df9e16f87f4efada768d4913cecaaaddb931
carlosp2001 <pinedalberto@hotmail.com>
2023-11-22 13:03:05 -0600

    Contextos e Inyección de Dependencias (CDI) usando Jakarta en versión de Spring Boot 3

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/cdi/SomeCdiBusiness.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.basics.springin5steps.cdi;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// Si la versión de Spring Boot es 3,debemos utilizar jakart.inject en vez
+// de javax.inject
+@Named // A cambio de usar @Component podemos usar @Named
+public class SomeCdiBusiness {
+    @Inject
+    SomeCdiDao someCdiDao;
+
+    public SomeCdiDao getSomeCdiDao() {
+        return someCdiDao;
+    }
+
+    public void setSomeCDIDAO(SomeCdiDao someCdiDao) {
+        this.someCdiDao = someCdiDao;
+    }
+}
---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/cdi/SomeCdiDao.java
@@ -0,0 +1,8 @@
+package com.in28minutes.spring.basics.springin5steps.cdi;
+
+import jakarta.inject.Named;
+import org.springframework.stereotype.Component;
+
+@Named
+public class SomeCdiDao {
+}
