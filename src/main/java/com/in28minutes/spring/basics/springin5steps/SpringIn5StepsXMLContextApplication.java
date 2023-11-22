99118527a1e8fe5745cc1b6d5d075828ffb9828e
carlosp2001 <pinedalberto@hotmail.com>
2023-11-22 14:27:29 -0600

    Mezclando contexto con XML y componentScan para cargar los componentes automáticamente

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -0,0 +1,23 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import com.in28minutes.spring.basics.springin5steps.xml.XmlPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringIn5StepsXMLContextApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsXMLContextApplication.class);
+
+    public static void main(String[] args) {
+
+        try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml")) {
+
+            LOGGER.info("Beans Loaded ->  {}", (Object)applicationContext.getBeanDefinitionNames());
+
+            XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);
+            System.out.println(personDao);
+            System.out.println(personDao.getXmlJdbcConnection());
+        }
+    }
+}
