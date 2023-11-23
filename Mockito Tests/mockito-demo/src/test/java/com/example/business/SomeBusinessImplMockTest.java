332d971193ee53f12ebdcec338efba60cd3cba65
carlosp2001 <pinedalberto@hotmail.com>
2023-11-23 08:27:22 -0600

    Simplificando pruebas con mokito, utilizando anotaciones @ExtendWith, @Mock, @InjectMocks

--- Mockito Tests/mockito-demo/src/test/java/com/example/business/SomeBusinessImplMockTest.java
+++ Mockito Tests/mockito-demo/src/test/java/com/example/business/SomeBusinessImplMockTest.java
@@ -1,28 +1,40 @@
 package com.example.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
+
     @Test
     void findTheGreatestFromAllData_basicScenario() {
-        DataService dataServiceMock = mock(DataService.class);
-//        dataServiceMock.retrieveAllData() => new int[]{25, 15, 5}
+//        DataService dataServiceMock = mock(DataService.class);
+////        dataServiceMock.retrieveAllData() => new int[]{25, 15, 5}
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+//        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
 
         assertEquals(25, businessImpl.findTheGreatestFromAllData());
     }
 
     @Test
     void findTheGreatestFromAllData_OneValue() {
-        DataService dataServiceMock = mock(DataService.class);
+//        DataService dataServiceMock = mock(DataService.class);
 //        dataServiceMock.retrieveAllData() => new int[]{25, 15, 5}
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{35});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+//        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
 
         assertEquals(35, businessImpl.findTheGreatestFromAllData());
     }
