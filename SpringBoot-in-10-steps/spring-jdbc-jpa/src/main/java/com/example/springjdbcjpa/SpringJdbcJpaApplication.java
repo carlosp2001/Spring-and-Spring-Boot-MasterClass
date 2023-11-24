3644e3a27dd30c81d93c2da2d4f2336da025b52e
carlosp2001 <pinedalberto@hotmail.com>
2023-11-24 08:34:02 -0600

    Creando un row mapper personalizado con JDBC Row Mapper

--- SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/SpringJdbcJpaApplication.java
+++ SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/SpringJdbcJpaApplication.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 @SpringBootApplication
--- SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/jdbc/PersonJdbcDao.java
+++ SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.example.springjdbcjpa.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,23 +18,59 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    // select * from person
-    public List<Person> findAll() {
-        return jdbcTemplate.query("select * from person",
-                new BeanPropertyRowMapper<Person>(Person.class));
+    /**
+     * Row mapper para personalizar la obtención de datos para el modelo Persona
+     */
+    class PersonRowMapper implements RowMapper<Person> {
+        // Los row mappers nos ayudan en el caso de que las columnas en las tablas sean muy diferentes a como lo
+        // tenemos definido en nuestro modelo
+        // En este caso podemos buscar vincular cada campo de fila
+        @Override
+        public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Person person = new Person();
+            person.setId(rs.getInt("id"));
+            person.setName(rs.getString("name"));
+            person.setLocation(rs.getString("location"));
+            person.setBirthDate(rs.getTimestamp("birth_date"));
+            return person;
+        }
     }
 
+    /**
+     * Método para retornar todos los registros de personas
+     * @return
+     */
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person",
+                new PersonRowMapper());
+    }
+
+    /**
+     * Método en el Data Access Object para encontrar un id
+     * @param id
+     * @return
+     */
     public Person findById(int id) {
         return jdbcTemplate.queryForObject("select * from person where id=?",
                 new Object[]{id},
                 new BeanPropertyRowMapper<Person>(Person.class));
     }
 
+    /**
+     * Método en el DAO para eliminar un id
+     * @param id
+     * @return
+     */
     public int deleteById(int id) {
         return jdbcTemplate.update("delete from person where id=?",
                 new Object[]{id});
     }
 
+    /**
+     * Método en el DAO para insertar una nueva persona
+     * @param person
+     * @return
+     */
     public int insert(Person person) {
         return jdbcTemplate.update(
                 "INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) " +
@@ -42,6 +81,11 @@ public class PersonJdbcDao {
                 });
     }
 
+    /**
+     * Método en el DAO para actualizar un registro de una persona
+     * @param person
+     * @return
+     */
     public int update(Person person) {
         return jdbcTemplate.update(
                 "UPDATE PERSON " +
