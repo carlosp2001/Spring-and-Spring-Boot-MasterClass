69f6efda3f4fc90b60136c89d7b2a5dd4630192c
carlosp2001 <pinedalberto@hotmail.com>
2023-11-24 08:09:59 -0600

    Implementando insert y update métodos de Spring JDBC Update

--- SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/SpringJdbcJpaApplication.java
+++ SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/SpringJdbcJpaApplication.java
@@ -1,5 +1,6 @@
 package com.example.springjdbcjpa;
 
+import com.example.springjdbcjpa.entity.Person;
 import com.example.springjdbcjpa.jdbc.PersonJdbcDao;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringJdbcJpaApplication implements CommandLineRunner {
@@ -24,6 +27,11 @@ public class SpringJdbcJpaApplication implements CommandLineRunner {
         logger.info("All users -> {}", dao.findAll());
         logger.info("User id 10002 -> {}", dao.findById(10002));
         logger.info("Deleting 10001 -> No of Rows Deleted {}", dao.deleteById(10001));
+        logger.info("All users -> {}", dao.findAll());
+        logger.info("Inserting 10004 -> {}",
+                dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+        logger.info("Updating 10004 -> {}",
+                dao.update(new Person(10004, "Taras", "Paris", new Date())));
         logger.info("All users -> {}", dao.findAll());
     }
 }
--- SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/jdbc/PersonJdbcDao.java
+++ SpringBoot-in-10-steps/spring-jdbc-jpa/src/main/java/com/example/springjdbcjpa/jdbc/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository // @Repository es la anotación correcta cuando necesitamos contactarnos
@@ -29,5 +30,26 @@ public class PersonJdbcDao {
     public int deleteById(int id) {
         return jdbcTemplate.update("delete from person where id=?",
                 new Object[]{id});
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update(
+                "INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) " +
+                        "VALUES (?, ?, ?, ?)",
+                new Object[]{
+                        person.getId(), person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime())
+                });
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update(
+                "UPDATE PERSON " +
+                        "SET NAME=?, LOCATION=?, BIRTH_DATE=? " +
+                        "WHERE ID=?",
+                new Object[]{
+                        person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime()), person.getId(),
+                });
     }
 }
