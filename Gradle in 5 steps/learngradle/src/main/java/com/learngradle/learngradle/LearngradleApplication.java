5f8fc820d03fb3cda022e710eb4df4f7dd56077f
carlosp2001 <pinedalberto@hotmail.com>
2023-11-26 13:32:03 -0600

    Comenzando a usar gradle

---
+++ Gradle in 5 steps/learngradle/src/main/java/com/learngradle/learngradle/LearngradleApplication.java
@@ -0,0 +1,13 @@
+package com.learngradle.learngradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearngradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearngradleApplication.class, args);
+    }
+
+}
---
+++ Gradle in 5 steps/learngradle/src/test/java/com/learngradle/learngradle/LearngradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngradle.learngradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
