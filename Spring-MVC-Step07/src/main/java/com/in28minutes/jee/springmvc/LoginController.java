6bb5b5f383902cd3a5746e0403da81f958221ea8
carlosp2001 <pinedalberto@hotmail.com>
2023-11-26 11:57:30 -0600

    Redireccionando a la página de bienvenida:ModelMap y @RequestParam

--- Spring-MVC-Step07/src/main/java/com/in28minutes/jee/springmvc/LoginController.java
+++ Spring-MVC-Step07/src/main/java/com/in28minutes/jee/springmvc/LoginController.java
@@ -1,16 +1,27 @@
 package com.in28minutes.jee.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value="/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     // @ResponseBody responseBody solo funciona si queremos que el texto se muestre literalmente
     // para mostrar una vista solo debemos colocar el nombre de la vista
-    public String sayHello() {
+    public String showLogginPage() {
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleLoginRequest(@RequestParam String name,
+                                     @RequestParam String password,
+                                     ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
